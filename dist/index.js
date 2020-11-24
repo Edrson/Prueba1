ecf62716429caf48f61dca9987d35b6be62a9b69
Edrson <ederson.ramirez87@gmail.com>
2020-11-24 01:15:02 -0600

    commit inicial de hola mundo

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+class Mensaje {
+    Imprimir() {
+        let i = 12;
+        i = 50;
+        i = 30;
+        i = 20;
+        console.log('1 Hello world from a class.');
+        console.log('2 Hello world from a class.');
+        console.log('3 Hello  world from a class.');
+        console.log('4 Hello  world from a class.');
+    }
+}
+let m = new Mensaje();
+m.Imprimir();
+console.log('ya');
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+class Mensaje{
+    Imprimir(){
+        let i : number = 12;
+        i = 50;
+        i = 30;
+        i = 20;
+        console.log('1 Hello world from a class.')
+        console.log('2 Hello world from a class.')
+        console.log('3 Hello  world from a class.')
+        console.log('4 Hello  world from a class.')
+    }
+}
+let m = new Mensaje();
+m.Imprimir();
+console.log('ya');
